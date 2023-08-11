3b7cf5f9122b0e8dd7eb59ec0e8d7a97d5d86927
Zeeshan17CO50 <mohammedsiddiquezee@gmail.com>
2023-08-11 12:41:00 +0530

    Implement basic UI Design

--- src/app/services/bubble-sort.service.ts
+++ src/app/services/bubble-sort.service.ts
@@ -2,47 +2,47 @@ import { Injectable } from '@angular/core';
 import { ArrayWithColor } from '../interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BubbleSortService {
+  constructor() {}
 
-  constructor() { }
-
-   async sort(array: ArrayWithColor[], speed: number): Promise<void> {
+  async sort(array: ArrayWithColor[], speed: number): Promise<void> {
     const n = array.length;
     let swapped: boolean;
-    speed = 1001-speed;
+    speed = 1001 - speed;
 
-    for(let i = 0; i < n-1; i++){
+    for (let i = 0; i < n - 1; i++) {
       swapped = false;
 
-      for(let j = 0; j < n-i-1; j++){
+      for (let j = 0; j < n - i - 1; j++) {
         array[j].color = 'green';
-        array[j+1].color = 'green';
+        array[j + 1].color = 'green';
         await this.delay(speed);
-        
-        if(array[j].value > array[j+1].value){
+
+        if (array[j].value > array[j + 1].value) {
           array[j].color = 'red';
-          array[j+1].color = 'red';
+          array[j + 1].color = 'red';
           await this.delay(speed);
-          
+
           const temp = array[j].value;
-          array[j].value = array[j+1].value;
-          array[j+1].value = temp;
+          array[j].value = array[j + 1].value;
+          array[j + 1].value = temp;
           swapped = true;
-          
+
           await this.delay(speed);
         }
         array[j].color = 'blue';
-        array[j+1].color = 'blue';
+        array[j + 1].color = 'blue';
       }
-      array[n-i-1].color = 'purple';
-      
-      if(!swapped){
-        for(let k = 0; k < n; k++) array[k].color = 'purple';
+      array[n - i - 1].color = 'purple';
+
+      if (!swapped) {
+        for (let k = 0; k < n; k++) array[k].color = 'purple';
         break;
       }
     }
+    array[0].color = 'purple';
   }
 
   private delay(ms: number): Promise<void> {
